899ec37b19098442a6dd51a60478a5cc6f500a65
Prabhubalu <im.prabhub@gmail.com>
2025-11-28 20:20:30 +0530

    fixed tab swapping behaviour

--- dev.ts
+++ dev.ts
@@ -6,4 +6,8 @@
 import './packages/vds-button/vds-button.js';
 import './packages/vds-input/vds-input.js';
 import './packages/vds-modal/vds-modal.js';
+import './packages/vds-tab-item/vds-tab-item.js';
+import './packages/vds-tab/vds-tab.js';
+import './packages/vds-menu/vds-menu.js';
+import './packages/vds-menu-item/vds-menu-item.js';
 
